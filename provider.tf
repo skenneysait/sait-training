7e57c936ed889a7d22907bebda10126b99e1276c
Matt Cole <matt.cole@benchmarkcorp.com>
2021-11-24 17:16:53 -0500

    test

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
 provider "azurerm" {
-
   features {}
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
