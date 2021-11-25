355991b54ed9601ab13b379fbaf6cc967e0064cc
Matt Cole <matt.cole@benchmarkcorp.com>
2021-11-25 06:48:22 -0500

    added ansible

--- compute.tf
+++ compute.tf
@@ -82,7 +82,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
 
 data "azurerm_public_ip" "myterraformpublicip" {
   name                = azurerm_public_ip.myterraformpublicip.name
-  resource_group_name = azurerm_virtual_machine.myterraformpublicip.resource_group_name
+  resource_group_name = "TrainingResourceGroup"
 }
 
 output "public_ip_address" {
