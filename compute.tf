33de3ee168cd72d148de883d6a0d57a086ad9248
skenneysait <89872458+skenneysait@users.noreply.github.com>
2022-02-07 11:51:27 -0700

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -3,3 +3,8 @@ data "azurerm_subnet" "myterraformsubnet" {
   virtual_network_name = "TF-ResourceGroup-vnet"
   resource_group_name  = "TF-ResourceGroup"
 }
+
+data "azurerm_storage_account" "myterraformstorageaccount" {
+  name                = "tfstorageaccount161"
+  resource_group_name = "TF-ResourceGroup"
+}
