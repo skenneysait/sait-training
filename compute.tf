d2237dce3504c6543b7417c41c00036f512afa32
skenneysait <89872458+skenneysait@users.noreply.github.com>
2022-02-07 12:07:53 -0700

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -40,3 +40,37 @@ resource "azurerm_network_interface" "myterraformnic" {
     environment = "Terraform Demo"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+  name                  = "TF-VM2"
+  location              = "Canada Central"
+  resource_group_name   = "TF-ResourceGroup"
+  network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "TF-VM2-OsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name                   = "TF-VM2"
+  disable_password_authentication = "false"
+  admin_username                  = var.ansible_user
+  admin_password                  = var.ansible_pass
+
+  boot_diagnostics {
+    storage_account_uri = data.azurerm_storage_account.example.primary_blob_endpoint
+  }
+
+  tags = {
+    environment = "Terraform Demo"
+  }
+}
