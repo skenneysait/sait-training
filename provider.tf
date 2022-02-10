82155d6f2d8232c628522474988f20a64e9f72cd
Shawn Kenney <shawn.kenney@sait.ca>
2022-02-10 15:46:59 +0000

    Starting to add steps for Terraform Backend

--- provider.tf
+++ provider.tf
@@ -13,3 +13,14 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
 }
+
+terraform {
+  #  #***************************************************
+  #  # Use GitHub as the Terraform Backend (with state locking)
+
+  backend "http" {
+    address        = "http://localhost:6061/?type=git&repository=git@github.com:skenneysait/terraform-backend.git&state=state.json&ref=main"
+    lock_address   = "http://localhost:6061/?type=git&repository=git@github.com:skenneysait/terraform-backend.git&state=state.json&ref=main"
+    unlock_address = "http://localhost:6061/?type=git&repository=git@github.com:skenneysait/terraform-backend.git&state=state.json&ref=main"
+  }
+}
