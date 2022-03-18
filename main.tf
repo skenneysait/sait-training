22d690083770b53782663479317407b0bb908982
Shawn Kenney <shawn.kenney@sait.ca>
2022-03-18 11:27:56 -0600

    whatever

--- main.tf
+++ main.tf
@@ -14,12 +14,11 @@ module "virtual-network" {
 }
 
 # Create subnet
-# resource "azurerm_subnet" "myterraformsubnet" {
-#   name                 = "mySubnet"
-#   resource_group_name  = var.resource_group
-#   virtual_network_name = "myVnet"
-#   address_prefixes     = ["10.0.1.0/24"]
-# }
+module "subnet" {
+  source         = "./modules/subnet"
+  resource_group = var.resource_group
+  depends_on     = [module.virtual-network]
+}
 
 # output "subnet_id" {
 #   value = data.azurerm_subnet.myterraformsubnet.id
