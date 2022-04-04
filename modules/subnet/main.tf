0c5c3b4149a6f691c9c777ef17cbe6cbdc5bd11e
Shawn Kenney <shawn.kenney@sait.ca>
2022-04-04 16:09:51 -0600

    Change virtual network name

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -2,6 +2,6 @@
 resource "azurerm_subnet" "myterraformsubnet" {
   name                 = "mySubnet"
   resource_group_name  = var.resource_group
-  virtual_network_name = "myVnet"
+  virtual_network_name = "S1SB1TST-myVnet"
   address_prefixes     = ["10.0.1.0/24"]
 }
--- modules/virtual-network/main.tf
+++ modules/virtual-network/main.tf
@@ -1,6 +1,6 @@
 # Create virtual network
 resource "azurerm_virtual_network" "myterraformnetwork" {
-  name                = "myVnet"
+  name                = "S1SB1TST-myVnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.node_location
   resource_group_name = var.resource_group
